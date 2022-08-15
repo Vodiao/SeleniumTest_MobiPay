2fad55ecf026cecca7e86c5f3a9cdbe8a2221c37
Vadym <vpotorzinskij@gmail.com>
2022-08-15 21:06:20 +0200

    SeleniumTest_MobiPay

---
+++ src/main/java/FirstTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+
+    @Test
+     public void check(){
+
+
+
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver() ;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://next.privat24.ua/mobile");
+
+
+        By phoneNumber = By.xpath("//input[@data-qa-node='phone-number']");
+        By amount = By.xpath("//input[@data-qa-node='amount']");
+
+        By numberCard = By.xpath("//input[@data-qa-node='numberdebitSource']");
+        By expDate = By.xpath("//input[@data-qa-node='expiredebitSource']");
+        By cvv = By.xpath("//input[@data-qa-node='cvvdebitSource']");
+        By submitBtn = By.xpath("//button[@data-qa-node='submit']");
+        By name = By.xpath("//input[@data-qa-node='firstNamedebitSource']");
+        By surname = By.xpath("//input[@data-qa-node='lastNamedebitSource']");
+
+
+
+
+        driver.findElement(phoneNumber).sendKeys("677013326");
+        driver.findElement(amount).sendKeys(Keys.CONTROL+"A");
+        driver.findElement(amount).sendKeys("55");
+        driver.findElement(numberCard).sendKeys("4552331448138217");
+        driver.findElement(expDate).sendKeys("1129");
+        driver.findElement(cvv).sendKeys("112");
+        driver.findElement(name).sendKeys("Vadym");
+        driver.findElement(surname).sendKeys("Potorzhynkyi");
+        driver.findElement(submitBtn).submit();
+
+
+
+    }
+}
